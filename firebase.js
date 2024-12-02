36ec82c7a0f07e2972b7eb2223c698d27705e90d
med <131326765+Dgn999@users.noreply.github.com>
2024-12-02 14:29:42 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Firebase Configuration
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBknFT-_HpQFiVp33oq9T-aDTtZeR64Ug",
+  authDomain: "mi-salud-programada.firebaseapp.com",
+  projectId: "mi-salud-programada",
+  storageBucket: "mi-salud-programada.appspot.com",
+  messagingSenderId: "308259898816",
+  appId: "1:308259898816:web:922106bac7f75473a07425",
+  measurementId: "G-Z59R7FV0R0"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
